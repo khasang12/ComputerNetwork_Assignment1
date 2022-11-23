09cbef5c4ff1a2aebfe74bc8442c48fa53e5058f
Kha Sang <sang.khask12@hcmut.edu.vn>
2022-11-23 19:23:03 +0700

    server code - online users and logout done

--- utils/peer.py
+++ utils/peer.py
@@ -1,9 +1,10 @@
-import threading
+import threading, time
 import socket, select
 from tkinter import *
 from tkinter import font
 from tkinter import ttk
-import sys
+import sys,json
+from protocol import Encode
 FORMAT = "utf-8"
 
 
@@ -11,38 +12,44 @@ FORMAT = "utf-8"
 # This will contain an Array of All Client Connection
 # This Will Act As A Chat Room
 class PeerServer(threading.Thread):
-    ClientList = {} # List Of Client Connection IP:Connection 
+    ClientList = [] # List Of Client Connection IP:Connection 
     peerIP = None
     peerPort = None
     def __init__(self,peerIp,peerPort) -> None:
         threading.Thread.__init__(self)
         self.CliendList = []
         self.peerIP = peerIp
-        self.peerPort = peerPort
+        self.listenPort = peerPort
 
         self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.server.bind((peerIp, peerPort))
+        self.server.bind((peerIp, self.listenPort))
+
 
     def run(self):
         self.server.listen(100)
-        print("Start Listening on Port: {}".format(port))
+        print("Start Listening on Port: {}".format(self.listenPort))
         inputs = [self.server]
 
         # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
         # Comment This one To act as Client because Server Thread will Take place std in
-        while(inputs):
-            readable, writable, exceptional = select.select(inputs, [], [])
-            for s in readable:
-                if s is self.server:
-                        # accepts the connection, and adds its connection socket to the inputs list
-                        # so that we can monitor that socket as well
-                        conn, addr = s.accept()
-                        self.createClientThread(conn)
+        # while(inputs):
+        #     try:
+        #         readable, writable, exceptional = select.select(inputs, [], [])
+        #         for s in readable:
+        #             if s is self.server:
+        #                     conn, addr = s.accept()
+        #                     self.createClientThread(conn,addr[0],addr[1])
+        #                     self.ClientList.append(conn)
+        #             else:
+        #                 print(s)
+        #     except Exception as e:
+        #         print(e)
 
-    def createClientThread(self,conn) :
+    def createClientThread(self,conn,cliIP, cliPort) :
         # Create A Thread For Handle That Connection
         # May be a new window
-        PeerClient(self.peerIP,conn).start()
+        print(conn)
+        PeerClient(self.peerIP,conn,cliIP,cliPort).start()
         self.CliendList.append(conn)
         pass
     def sendToAll(self,conn):
@@ -55,13 +62,18 @@ class PeerServer(threading.Thread):
 # This is Client Side Of the Peer
 # Request and send Message Chat to Other Peer
 # This handle Each Connection
+# This Class Maybe Handle Transfer
 class PeerClient(threading.Thread):
      # constructor method
-    def __init__(self, name,conn):
+    def __init__(self, name,conn,ip,port):
         threading.Thread.__init__(self)
         # chat window which is currently hidden
         self.name = name
         self.conn = conn
+        self.cip = ip
+        self.port = port
+        self.Encoder = Encode(ip,port)
+
 
 
     def run(self) :
@@ -69,6 +81,7 @@ class PeerClient(threading.Thread):
         self.Window.withdraw()
         self.goAhead(self.name)
         self.Window.mainloop()
+    
     
 
     def goAhead(self, name):
@@ -79,7 +92,6 @@ class PeerClient(threading.Thread):
  
     # The main layout of the chat
     def layout(self, name):
- 
         self.name = name
         # to show chat window
         self.Window.deiconify()
@@ -174,35 +186,49 @@ class PeerClient(threading.Thread):
         snd = threading.Thread(target=self.sendMessage)
         snd.start()
  
-    # function to receive messages
+    def displayMessage(self,msg):
+        self.textCons.config(state=NORMAL)
+        self.textCons.insert(END,
+                                msg+"\n\n")
+
+        self.textCons.config(state=DISABLED)
+        self.textCons.see(END)
+     
+
+
+    # function to receive messages 
+    # This will handle request send from user
     def receive(self):
         while True:
             try:
                 message = self.conn.recv(1024).decode(FORMAT)
- 
-                # if the messages from the server is NAME send the conn's name
-                if message == 'NAME':
-                    self.conn.send(self.name.encode(FORMAT))
-                else:
+                message = json.loads(message)
+                # If message is request message
+                if message['type'] == 'Request':
+                    # If it is Start Chat Request
+                    if message['flag'] == "S":
+                        # Call UIn   
+                        diaglogResult = messagebox.askokcancel("There is Message Request","nah")
+                        if(diaglogResult == "yes"):
+                            self.conn.send(self.Encoder.acceptChat())
+                        else:
+                            self.conn.send(self.Encoder.declineChat())
+                            self.Window.destroy()
+                elif  message['type'] == 'M':
                     # insert messages to text box
-                    self.textCons.config(state=NORMAL)
-                    self.textCons.insert(END,
-                                         message+"\n\n")
- 
-                    self.textCons.config(state=DISABLED)
-                    self.textCons.see(END)
+                    self.displayMessage("("+message["time"]+"):"+message['msg'])
             except:
                 # an error will be printed on the command line or console if there's an error
                 print("An error occurred!")
                 self.conn.close()
                 break
- 
+
     # function to send messages
     def sendMessage(self):
         self.textCons.config(state=DISABLED)
         while True:
-            message = (f"{self.name}: {self.msg}")
-            self.conn.send(message.encode(FORMAT))
+            message = (f"{self.name}: {self.msg}")           
+            self.conn.send(self.Encoder.sendMessage(message))
             break
 
 
@@ -216,22 +242,251 @@ class Peer:
     def __init__(self,peerIp, peerPort) -> None:
         self.peerIp = peerIp
         self.peerPort = peerPort
+        self.Encoder = Encode(self.peerIp, self.peerPort)
+
+        UI = threading.Thread(target=self.runUI)
+        UI.start()
         self.HandleConnection=PeerServer(peerIp,peerPort)
-        self.HandleConnection.run()
+        self.HandleConnection.daemon=True
+        self.HandleConnection.start()
+    
+
 
     def run(self):
         while(True):
             choose = input("Enter Your Choose")
-
             if(choose == "1" ):
                 [ip,port] = input("Connect to IP Port: ").strip().split(" ")
                 conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
                 conn.connect((ip, int(port)))
-                self.HandleConnection.createClientThread(conn)
+
+                conn.send(self.Encoder.requestChat())
+
+                msg = conn.recv(1024).decode(FORMAT)
+                msg = json.loads(msg)
+                if(msg['code']==1):
+                    self.HandleConnection.createClientThread(conn,ip,port)
+                else:
+                    print("Peer Decline Chat")
 
 
+    # This is for testing purpose
+    def runUI(self): 
+        self.Window = Tk()
+        self.Window.withdraw()
+        self.goAhead("Haha")
+        self.Window.mainloop()
+
+    def goAhead(self, name):
+        self.layout(name)
+        # the thread to receive messages
+ 
+    # The main layout of the chat
+    def layout(self, name):
+ 
+        self.name = name
+        # to show chat window
+        self.Window.deiconify()
+        self.Window.title("Hello User {}".format(self.peerIp))
+
+        self.Window.configure(width=470,
+                              height=150,
+                              bg="#17202A")
+                              
+        self.labelBottom = Label(self.Window,
+                                 bg="#ABB2B9",
+                                 height=80)
+ 
+        self.labelBottom.place(relwidth=1,
+                               rely=0)
+ 
+        self.entryMsg = Entry(self.labelBottom,
+                              bg="#2C3E50",
+                              fg="#EAECEE",
+                              font="Helvetica 13")
+ 
+        # place the given widget
+        # into the gui window
+        self.entryMsg.place(relwidth=0.74,
+                            relheight=0.06,
+                            rely=0.008,
+                            relx=0.011)
+ 
+        self.entryMsg.focus()
+ 
+        # create a Send Button
+        self.buttonMsg = Button(self.labelBottom,
+                                text="Send",
+                                font="Helvetica 10 bold",
+                                width=20,
+                                bg="#ABB2B9",
+                                command=lambda: self.sendButton(self.entryMsg.get()))
+ 
+        self.buttonMsg.place(relx=0.77,
+                             rely=0.008,
+                             relheight=0.06,
+                             relwidth=0.22)
+ 
+    # function to basically start the thread for sending messages
+    def sendButton(self, msg):
+        self.msg = msg
+        self.entryMsg.delete(0, END)
+        [ip,port] = self.msg.strip().split(" ")
+        conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        conn.connect((ip, int(port)))
+        conn.send(Encode.requestChat())
+        conn.recv(1024)
+        self.HandleConnection.createClientThread(conn,ip,port)
+
+
+
+# This is The Peer Main Class act as A routing
+# It will contain Server side and Client Side
+# This peer will listen request sent from other Peer
+START_CHECKING = False
+class Peer_Central(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+        self.HOST = 'localhost'
+        self.PORT_TCP = 3000
+        self.PORT_UDP = 3004
+        self.central_client_socket = None
+        self.userName = None
+        self.password = None
+        self.ip_addr = None
+        self.socket = None
+        self.CONDITION = True
+        self.startTime = 0
+        self.endTime = 0
+        self.running = 1
+    
+    def run(self):
+        # Create central socket - TCP port
+        self.central_client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        try:
+            self.central_client_socket.connect((self.HOST, self.PORT_TCP))
+        except:
+            print('Unable connection to central server unit')
+        
+        # Maintain online
+        threading.Thread(target=self.checkConn).start()
+            
+        # Choose
+        while self.running == 1:
+            while self.CONDITION:
+                print ("\n---------------------------------------------------")
+                choice = str(input("\rPlease, press '1' for registration, '2' for join: "))
+                print ("---------------------------------------------------\n")
+                if choice == "1":
+                    self.registerClient()
+                    break
+                elif choice == "2":
+                    self.loginClient()
+                    self.CONDITION = False
+                    #self.kill()
+                    break
+                else:
+                    print("1 or 2 only ! \n ")
+            time.sleep(0)
+    
+    def runChatBox(self,otherIP, otherPort):
+        # open server
+        self.HandleConnection=PeerServer(self.ip_addr,self.socket)
+        self.HandleConnection.run()
+        # 
+    
+    def checkConn(self):
+        self.checkConn = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+        while True:
+            if(START_CHECKING):
+                self.endTime = time.time()
+                elapsedTime = int(self.endTime - self.startTime)
+                # send to server after 3 secs
+                if (elapsedTime % 3) == 0:
+                    data = str(self.userName + "," + "Hello")
+                    self.checkConn.sendto(data.encode(), (self.HOST, int(self.PORT_UDP)))
+            time.sleep(0.1)
+    
+    def registerClient(self):
+        self.central_client_socket.send("register".encode())
+        
+        # register validation stuff (client side)
+        self.userName = str(input("UserName: "))
+        self.password = str(input("Password: "))
+        data = str(self.userName + "," + self.password)
+        
+        self.central_client_socket.send(data.encode())
+        processStatus = self.central_client_socket.recv(1024).decode()
+        print(processStatus)
+    
+    def loginClient(self):
+        host_name = socket.gethostname()
+        ip_addr = socket.gethostbyname(host_name)
+        sock = socket.socket()
+        sock.bind(('', 0))
+        free_sock = sock.getsockname()[1]
+        
+        # login validation stuff (client side)
+        self.central_client_socket.send("login".encode())
+        self.userName = str(input("UserName: "))
+        self.password = str(input("Password: "))
+        self.ip_addr = str(ip_addr)
+        self.socket = str(free_sock)
+        
+        data = str(self.userName + "," + self.password+","+self.ip_addr+","+self.socket)
+        
+        self.central_client_socket.send(data.encode())
+        processStatus = self.central_client_socket.recv(1024).decode()
+        print(processStatus)
+        
+        # Assume that login is complete
+        global START_CHECKING 
+        START_CHECKING = True
+        self.startTime = time.time()
+        
+        while True:
+            # Greeting...
+            print(f"\n Welcome {self.central_client_socket.recv(1024).decode()}")
+            
+            # Show online users
+            self.central_client_socket.send("onlineList".encode())
+            time.sleep(0.2)
+            data = self.central_client_socket.recv(4096).decode()
+            printOnlineUsers(data)
+            
+            
+            while 1:
+                user = input("\rType Username you want to chat with: ")
+                self.central_client_socket.send("search".encode())
+                self.central_client_socket.send(user.encode())
+                peer_ip,peer_port = self.central_client_socket.recv(1024).decode().split(",")
+                # Open Box Chat from Peer to Peer
+                break
+            break
+        
+def printOnlineUsers(data):
+    print ("\t\t<<< ONLINE USER LIST >>>\r")
+    print ("\t UserName    IpAddress    Port\n")
+    onlineList = data[1:-1].replace("(", "").replace(")", "").replace(",", "").replace("'", "").split(" ")
+    iteration = 1
+    userAllInfo = ""
+    for userInfo in onlineList:
+        if (iteration%3) == 0:
+            print("\t" + userAllInfo + userInfo + ",   ")
+            userAllInfo = ""    
+        else:
+            userAllInfo = userAllInfo + userInfo + ",   "
+        iteration += 1
+                
 if __name__ == "__main__":
-    [ip,port] = input("IP Port: ").strip().split(" ")
-    Peer(ip,int(port)).run()
+    # [ip,port] = input("IP Port: ").strip().split(" ")
+    # Peer(ip,int(port)).run()
+    peer_central = Peer_Central()
+    peer_central.start()
+    
+    #[ip,port] = input("IP Port: ").strip().split(" ")
+    # ip = socket.gethostbyname(socket.gethostname())
+    #Peer(ip,int(port)).run()
 
 
--- utils/server.py
+++ utils/server.py
@@ -1,24 +1,284 @@
+import sys
+from threading import Thread
+import traceback
+
 import csv
 import tkinter.messagebox
+import socket
+from peer import *
+import sqlite3
 
-#### SIGNIN - SIGNUP
+
+
+# TCPServer: Must be Opened before any peer connection begins
+class TCPServer(threading.Thread):
+    SOCKET_LIST = []
+    def __init__(self):
+        threading.Thread.__init__(self)
+        self.HOST = ''
+        self.PORT = 3000
+        self.server_socket = None
+        self.running = 1
+
+    def run(self):
+        # TCP socket              
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) 
+
+        self.server_socket.bind((self.HOST, self.PORT))
+        self.server_socket.listen(100)
+        
+        self.SOCKET_LIST = [self.server_socket] # self-added
+        
+
+        print("TCP Server started on port " + str(self.PORT) + "\n")
+
+        while 1:
+            try:
+                ready_to_read, _, _ = select.select(self.SOCKET_LIST, [], [], 0)
+            except:
+                self.SOCKET_LIST =  filter(lambda item: item.fileno > 0, self.SOCKET_LIST)
+                continue
+            for sock in ready_to_read:
+                if sock == self.server_socket:
+                    self.conn, self.addr = self.server_socket.accept()
+                    self.SOCKET_LIST.append(self.conn)
+                    print("Client (%s, %s) connected" % self.addr)
+                    thread = CentralServer(self.conn, self.addr)
+                    thread.start()
+        
+        # UDP Socket
+
+    def kill(self):
+        self.running = 0
+
+# Central Server: "Threads" created by TCPServer to handle TCP operations
+class CentralServer(threading.Thread):
+    def __init__(self,conn,addr) -> None: # conn and addr are client's
+        threading.Thread.__init__(self)
+        self.ClientList = [] # List of Online Clients
+        self.conn = conn
+        self.addr = addr
+        self.running = 1
+
+    # Define available types of request from peers
+    def run(self): 
+        self.initDatabaseConn()
+        print(f"Server Start Listening on: {self.addr}")
+        while self.running: # while still running
+            try:
+                request = self.conn.recv(1024).decode()
+                print(f"Client request to Server: {request}")
+                # Registration Service
+                if request == "register":
+                    self.registerService()
+                # Join Service - Update user status and provide new IP-Port
+                elif request == "login":
+                    self.loginService()
+                # Search Service - Find a person to chat with
+                elif request == "search":
+                    self.searchService()
+                # Retrieve Online List Service - Get online contacts
+                elif request == "onlineList":
+                    self.getOnlineUsersService()
+                else:
+                    print("Invalid Request")
+            except:
+                print (f"Client {self.addr} is not online or having connection errors !!")
+                traceback.print_exc()
+                break
+    
+    # IMPLEMENTATION OF SERVER'S SERVICES
+    
+    def registerService(self):
+        # we used forloop to ensure registration will always be fulfilled
+        while 1:
+            user,passwd = self.conn.recv(1024).decode().split(',')
+            record = self.getAccountByUsername(user)
+            if record != []:
+                sendMsg(self.conn, "Account has been used")
+                break
+            else:
+                try:
+                    self.insertUser(user,passwd)
+                    sendMsg(self.conn, "Account created successfully")
+                    break
+                except:
+                    sendMsg(self.conn, "Error. Try again...")
+                    traceback.print_exc()
+                    break
+
+    def loginService(self):
+        while 1:
+            user,passwd,ip,port = self.conn.recv(1024).decode().split(',')
+            print(user)
+            record = self.getAccountByUsernameAndPassword(user,passwd)
+            if record == None:
+                sendMsg(self.conn, "Tài khoản hoặc mật khẩu không tồn tại")
+                break
+            else:
+                try:
+                    self.updateUser(user,passwd,ip,port)
+                    sendMsg(self.conn, "Kết nối thành công")
+                    sendMsg(self.conn,user)
+                    break
+                except:
+                    sendMsg(self.conn, "Lỗi truy vấn. Đang thử lại...")
+                    traceback.print_exc()
+                    break
+    
+    def searchService(self):
+        while 1:
+            user = self.conn.recv(1024).decode()
+            record = self.getAddressByUsername(user)
+            if record == None:
+                sendMsg(self.conn, "Tài khoản tìm kiếm không tồn tại")
+                break
+            else:
+                # db indices: 3-ip, 4-port, 5-status
+                if (record != []):
+                    data = str(record[0][0]+","+record[0][1])
+                    sendMsg(self.conn,data)
+                    break
+                else:
+                    sendMsg(self.conn, "Tài khoản đang offline")
+                    break
+    
+    def getOnlineUsersService(self):
+        records = self.getAccountsOnline()
+        sendMsg(self.conn,str(records))
+        
+    # Database-Related Methods
+    
+    def initDatabaseConn(self):
+        self.connector = sqlite3.connect('accounts.db')
+        self.cursor = self.connector.cursor()
+    
+    def closeDatabaseConn(self):
+        self.cursor.close()
+        
+    def getAccountByUsername(self,user):
+        self.cursor.execute(f"""SELECT * FROM users WHERE username='{user}'""")
+        records = self.cursor.fetchall()
+        return records
+    
+    def getAccountByUsernameAndPassword(self,user,passwd):
+        self.cursor.execute(f"""SELECT * FROM users WHERE username='{user}' AND password='{passwd}'""")
+        records = self.cursor.fetchall()
+        return records
+    
+    def getAccountsOnline(self):
+        self.cursor.execute("SELECT * FROM users WHERE status=1")
+        records = self.cursor.fetchall()
+        return records
+
+    def getAddressByUsername(self,user):
+        self.cursor.execute(f"""SELECT ip,port FROM users WHERE username='{user}' AND status=1""")
+        records = self.cursor.fetchall()
+        print(records)
+        return records
+    
+    def insertUser(self,user,passwd):
+        self.cursor.execute(f"""INSERT INTO users (username,password,status) VALUES ('{user}','{passwd}',0)""")
+        self.connector.commit()
+    
+    def updateUser(self,user,passwd,ip,port):
+        self.cursor.execute(f"""UPDATE users SET status={1},ip='{ip}',port='{port}' WHERE username='{user}'""")
+        self.connector.commit()
+
+# UDPServer: One Server Thread to handle ALl Client Check-ins.
+# Port: 4008
+class UDPServer(threading.Thread):
+    def __init__(self) -> None: # conn and addr are client's
+        threading.Thread.__init__(self)
+        self.HOST = ''
+        self.PORT = 3004
+        self.server_socket = None
+        self.running = 1
+        self.ONLINE_LIST = {}
+
+    # Define available types of request from peers
+    def run(self): 
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.server_socket.bind((self.HOST, self.PORT))
+
+        print("UDP server started on port " + str(self.PORT) + "\n")
+        
+        threading.Thread(target=self.peerStatusCheck).start()
+
+        while self.running == 1:
+            try:
+                userNameData = self.server_socket.recv(1024).decode()
+                userName, data = str(userNameData).split(",")
+                
+                if data == "Hello":
+                    current = time.time()
+                    self.ONLINE_LIST[userName] = current
+                else:
+                    print("Wrong UDP data format \n")
+            except:
+                print("UDP port error \n")
+                traceback.print_stack()
+
+    def peerStatusCheck(self):
+        while udpThread.running == 1:
+            for name in self.ONLINE_LIST.copy().keys(): # create a copy to prevent edit-on-iterate error
+                elapsedTime = int(time.time() - self.ONLINE_LIST[name])
+                if elapsedTime > 5:  # no response for more than 5 secs
+                    print("Peer is offline -> '" + str(name) + "'\n") # print to server console
+                    try:
+                        self.ONLINE_LIST.pop(name)
+                    except:
+                        print("Online list update error")
+                    # update database
+                    self.updatePeerStatus(name)
+                    
+    def updatePeerStatus(self, userName):
+        self.connector = sqlite3.connect('accounts.db')
+        self.cursor = self.connector.cursor()  
+        self.cursor.execute(f"UPDATE users SET status = 0 WHERE username = '{str(userName)}'")
+        self.connector.commit()
+        self.connector.close()
+
+# sendMsg: Send message to a given connection
+def sendMsg(conn, msg):
+    try:
+        conn.send(msg.encode())
+    except:
+        conn.close()
+        
+#### Client side: SIGNIN - SIGNUP
 user_data_csv = "accounts.csv"
 def sign_in(user_name, password):
     try:
         # check if appears
+        print(user_name,password)
         check = False
         with open(user_data_csv, "rt") as f:
             csvreader = csv.reader(f, delimiter=",")
             for row in csvreader:
+                print(row)
                 if row == []:
                     break
-                elif user_name in row[0]:
+                elif user_name == row[0]:
                     check = True
                     if password != row[2]:
                         tkinter.messagebox.showerror(title="Lỗi đăng nhập",message="Nhập sai mật khẩu !!")
                         return
                     else:
                         tkinter.messagebox.showinfo(title="Chuyển hướng",message="Đăng nhập thành công !!")
+                        
+                        # Get your IP Address and Find available socket - picked by OS
+                        host_name = socket.gethostname()
+                        ip_addr = socket.gethostbyname(host_name)
+                        
+                        sock = socket.socket()
+                        sock.bind(('', 0))
+                        free_sock = sock.getsockname()[1]
+                        
+                        print(f"Login-> IP: {ip_addr}, Port: {free_sock}")
+                        Peer(ip_addr,int(free_sock)).run()
                         return
             if not check:
                 tkinter.messagebox.showerror(title="Lỗi đăng nhập",message="Tài khoản không tồn tại !!")
@@ -43,3 +303,9 @@ def sign_up(user_name, full_name, password):
         tkinter.messagebox.showinfo(title="Chuyển hướng",message="Đăng kí tài khoản thành công !!")
     except IOError:
         print("I/O error")
+
+if __name__ == "__main__":
+    tcpThread = TCPServer()
+    udpThread = UDPServer()
+    udpThread.start()
+    tcpThread.start()
